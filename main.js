0461a0b21f778924ea5c088318a2b428360e7df5
Denis774 <51455035+Denis774@users.noreply.github.com>
2019-06-19 08:39:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//Number Guessing Game
+
+var i = Math.floor((Math.random() * 100) + 1); //Denotes random numbers are generated and stored it in i
+var randomNumber = () => {
+    var i = Math.floor((Math.random() * 100) + 1); 
+    var j=i%2; //gets the modulus and stores the result in j
+    if (j==0) {//
+        alert ( "Number is Even"); }
+    else {
+        alert  ("Number is odd");
+    }
+}
+var enterNumber = () => {
+    for(var count=1;count<=10;count++)
+    {
+        let num = prompt ("Please enter a number:"); //Captures user input
+        if (num<1 || num>100){
+            alert ("Out of range");
+        }
+
+        if (num==i){
+            alert ("You WIN!");
+            break;
+        }
+        if (num<i){
+            alert ("Number is lower, make another guess");
+        }
+        if (num>i){
+            alert ("Number is higher,make another guess");
+                }
+        if (count==10){ 
+            alert ("You LOSE!");}
+    }
+}
+randomNumber();
+enterNumber();
